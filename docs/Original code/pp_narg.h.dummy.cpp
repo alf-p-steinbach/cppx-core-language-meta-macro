09c77bde4c999941e839f9766660141a94e1bd95
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-26 21:08:54 +0100

    Replaced with the evolved cppx versions

--- docs/Original code/pp_narg.h.dummy.cpp
+++
@@ -1,1 +0,0 @@
-#include "pp_narg.h"
--- source/cppx-core-language-meta-macro/MM_CONCAT.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-// Copyright (c) 2013 Alf P. Steinbach, distributed under Boost license 1.0.
-
-#define MM_CONCAT__( a, b )     a ## b
-#define MM_CONCAT_( a, b )      MM_CONCAT__( a, b )
-#define MM_CONCAT( a, b )       MM_CONCAT_( a, b )
