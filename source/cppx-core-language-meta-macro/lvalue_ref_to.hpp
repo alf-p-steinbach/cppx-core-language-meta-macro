59cf709434a37b94e472b7d02f7b4823e38dd399
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-14 18:46:20 +0100

    Handle calls to functions that return rvalue ref to const.

--- source/cppx-core-language-meta-macro/lvalue_ref_to.hpp
+++ source/cppx-core-language-meta-macro/lvalue_ref_to.hpp
@@ -2,12 +2,23 @@
 /// \file
 /// \brief Provides the `lvalue_ref_to` helper for restricting macro arguments.
 
-namespace cppx
+#include <type_traits>  // std::is_rvalue_reference_v
+
+namespace cppx::_
 {
     /// A helper for restricting macro arguments, used e.g. in `$items_of`.
     template< class Type >
-    inline auto lvalue_ref_to( Type& c )
+    inline auto lvalue_ref_to( Type&& c )
         -> Type&
-    { return c; }
+    {
+        using Argument_type = Type&&;
+        static_assert( not std::is_rvalue_reference_v<Argument_type> );
+        return c;
+    }
 
+}  // namespace cppx::_
+
+// Exporting namespace:
+namespace cppx {
+    using cppx::_::lvalue_ref_to;
 }  // namespace cppx
